142b012b875ecc04ad2938ab76e09497ec8ddc05
Muhammad Waseem <theprowaseem@gmail.com>
2022-11-06 01:31:16 +0500

    Working application code

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+function App(): JSX.Element {
+  return (
+    <Container fluid>
+      <Row className="header">
+        <Col>
+          Application Working
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default App;
